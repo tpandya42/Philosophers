afd2f85c8185b75743a914210588e7b234bb1364
Tanmay Pandya <tanmaypandya159@gmail.com>
2025-08-13 17:52:22 +0200

    added error check that input timestamps must be more than 60ms

--- philo/srcs/input_parser.c
+++ philo/srcs/input_parser.c
@@ -1,42 +1,43 @@
 #include "philo.h"
 
-static int parse_arg(const char *str, long *dest, char *err_msg)
+static int parse_number(const char *str, long *dest, int convert, char *err_msg)
 {
-	long tmp;
+    long tmp;
 
-	tmp = ft_atol(str); // will exit on error
-	if (tmp < 1)
-	{
-		write(2, err_msg, ft_strlen(err_msg));
-		write(2, "\n", 1);
-		return FAILURE;
-	}
-	*dest = tmp;
-	return SUCCESS;
+    tmp = ft_atol(str); 
+    if (tmp < 1)
+    {
+        write(2, err_msg, ft_strlen(err_msg));
+        write(2, "\n", 1);
+        return FAILURE;
+    }
+
+    *dest = tmp * convert; 
+    return SUCCESS;
 }
 
 int content(t_data *data, char **av, int ac)
 {
-	if (ac != 5 && ac != 6)
-	{
-		write(2, "Invalid number of arguments\n", 28);
-		return FAILURE;
-	}
-
-	if (parse_arg(av[1], &data->num_ph, "Invalid number_of_philosophers") ||
-			parse_arg(av[2], &data->ttd,    "Invalid time_to_die") ||
-			parse_arg(av[3], &data->tte,    "Invalid time_to_eat") ||
-			parse_arg(av[4], &data->tts,    "Invalid time_to_sleep"))
-		return FAILURE;
-
-	if (ac == 6)
-	{
-		if (parse_arg(av[5], &data->must_eat, "Invalid must_eat value"))
-			return FAILURE;
-	}
-	else
-		data->must_eat = -1;
-	printf("success from content\n");
-	return SUCCESS;
+    if (ac != 5 && ac != 6)
+    {
+        write(2, "Invalid number of arguments\n", 28);
+        return FAILURE;
+    }
+    if (parse_number(av[1], &data->num_ph, 1, "Invalid number_of_philosophers") ||
+        parse_number(av[2], &data->ttd,    1000, "Invalid time_to_die") ||
+        parse_number(av[3], &data->tte,    1000, "Invalid time_to_eat") ||
+        parse_number(av[4], &data->tts,    1000, "Invalid time_to_sleep"))
+        return FAILURE;
+    if (data->ttd < 6e4 || data->tte < 6e4 || data->tts < 6e4)
+	    return FAILURE;
+    if (ac == 6)
+    {
+        if (parse_number(av[5], &data->must_eat, 1, "Invalid must_eat value"))
+            return FAILURE;
+    }
+    else
+        data->must_eat = -1;
+    printf("Arguments parsed successfully!\n");
+    return SUCCESS;
 }
 
